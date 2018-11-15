5f56f7fa281a171b650a310dfcc440182cfced06
Richard Gomer <r.gomer@soton.ac.uk>
2018-11-15 15:49:36 +0000

    Start of flux model

---
+++ model.js
@@ -0,0 +1,10 @@
+/**
+ * A flux model for the modal shift UI
+ */
+
+const Dispatcher = require('flux').Dispatcher;
+
+var JourneyDispatcher = new Dispatcher();
+
+// Store the journey
+var JourneyStore = {};
